db5aaceda95bbb9a9fe5a9b90a4812145b290d50
codeArt001 <olaniyanoluwajomiloju25@gmail.com>
2025-09-27 12:16:06 +0100

    initial commit

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import myPhoto from "../assets/images/Untitled-1.jpg";
+import HTML from "../assets/images/html.png";
+import css from "../assets/images/CSS.jpeg";
+import script from "../assets/images/java.png";
+import act from "../assets/images/react.png";
 
 const About = () => {
   return (
@@ -11,18 +15,38 @@ const About = () => {
       />
       <div className="about-text fade-zoom-in">
         <p>
-          I’m{" "}
+          I’m {""}
           <span className="highlight">
-            {" "}
             <strong>Olaniyan Oluwajomiloju</strong>
           </span>
           , a passionate front-end developer with a love for building clean,
           functional, and user-friendly web applications. My journey into web
           development started with curiosity about how websites work, and today,
           I specialize in{" "}
-          <strong id="lang">
-            HTML, CSS, Tailwind CSS, JavaScript, and Reactjs.
-          </strong>
+          <div id="lang-container">
+            <div id="lang">
+              <img src={HTML} alt="" className="html-image" />
+              <div className="LANG1">HTML</div>
+            </div>
+            <div id="lang">
+              <img src={css} alt="" className="html-image" />
+              <div className="LANG1" id="css-text">
+                CSS
+              </div>
+            </div>
+            <div id="lang">
+              <img src={script} alt="" className="html-image" />
+              <div className="LANG1" id="java-text">
+                JAVASCRIPT
+              </div>
+            </div>
+            <div id="lang">
+              <img src={act} alt="" className="html-image" />
+              <div className="LANG1" id="react-text">
+                REACTJS
+              </div>
+            </div>
+          </div>
         </p>
         <p>
           I enjoy solving problems, turning ideas into interactive experiences,
